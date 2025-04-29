5089feaee9484d681521d3d2878d0843a184cd53
KayZin <kayzin6783@gmail.com>
2025-04-29 11:39:06 +0630

    Testing:color change

--- index.php
+++ index.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .red{
+            color:red,
+        }
+    </style>
 </head>
 <body>
     <h1>Hello world</h1>
+    <h6 class="red">Hi there</h6>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus perspiciatis odio blanditiis sed corrupti ipsum amet magnam a accusantium debitis, optio laudantium est animi soluta fuga vitae dolor deleniti libero.</p>
 </body>
 </html>
