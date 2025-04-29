718d76b64fcd06d320416cce8d5f5a3e57f63da5
KayZin <kayzin6783@gmail.com>
2025-04-29 10:55:46 +0630

    Testing:first

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus perspiciatis odio blanditiis sed corrupti ipsum amet magnam a accusantium debitis, optio laudantium est animi soluta fuga vitae dolor deleniti libero.</p>
+</body>
+</html>
